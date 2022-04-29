3c385981cf1ca7f78e6377e4a00cea7ad23013d8
skatingtamara <tamarabpatterson@gmail.com>
2022-04-28 22:02:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.waterme.viewmodel
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.work.*
+import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
+import kotlinx.coroutines.delay
+import java.util.concurrent.TimeUnit
+
+class PlantViewModel(application: Application): ViewModel() {
+
+    val plants = DataSource.plants
+
+    private val workManager = WorkManager.getInstance(application)
+
+    internal fun scheduleReminder(
+        duration: Long,
+        unit: TimeUnit,
+        plantName: String
+    ) {
+        // TODO: create a Data instance with the plantName passed to it
+
+        // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
+        //  instance
+
+        // TODO: Enqueue the request as a unique work request
+        val data = Data.Builder().putString(WaterReminderWorker.nameKey, plantName)
+
+        val oneTimeWorkRequest = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration,unit)
+            .setInputData(data.build())
+            .build()
+
+        workManager.enqueueUniqueWork(
+            plantName,
+            ExistingWorkPolicy.REPLACE,
+            oneTimeWorkRequest
+        )
+
+
+
+        }
+
+
+    }
+
+
+class PlantViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(PlantViewModel::class.java)) {
+            PlantViewModel(application) as T
+        } else {
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
+}
